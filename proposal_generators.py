2efb43dab4d643ea3471c022d3087fc7074a5b16
Spencer Young <spencermyoung513@gmail.com>
2023-04-17 14:06:57 -0600

    Added decay optionality to gaussian proposal generation, multivariate capabilities

--- proposal_generators.py
+++ proposal_generators.py
@@ -1,19 +1,46 @@
 import numpy as np
 from models import LinearLayer
+from scipy.stats import multivariate_normal as mv_norm
 
 class LinearLayerUnivariateNormalProposalGenerator:
     """Generates proposal parameters for a univariate linear layer (with parameters w and b).
     
     This object creates proposals by drawing from Gaussians centered at each parameter with some
     user-specified `stdev` that controls the variance in the distribution.
+
+    The `decay` parameter controls how the standard deviation of the proposal distribution shrinks
+    with time. With each successive call of `get_param_proposal`, self.stdev = self.stdev * decay.
+    Defaults to 1 (no decaying).
     """
 
-    def __init__(self, stdev: float = 1.1):
+    def __init__(self, stdev: float = 1., decay: float = 1.):
         self.stdev = stdev
+        self.decay = decay
 
     def get_param_proposal(self, model: LinearLayer) -> dict:
         """Get a parameter proposal from current model state."""
+        self.stdev *= self.decay
+
         proposed_w = np.random.normal(loc=model.w, scale=self.stdev)
         proposed_b = np.random.normal(loc=model.b, scale=self.stdev)
+
+        return {'w': proposed_w, 'b': proposed_b}
+    
+class LinearLayerMultivariateNormalProposalGenerator:
+    """Generates proposal parameters for a univariate linear layer (with parameters w and b).
+    
+    This object creates proposals by drawing from multivariate Gaussians centered at each parameter
+    with some user-specified `cov` that controls the covariance in the distribution.
+    """
+    def __init__(self, cov: float = 1., decay: float = 1.):
+        self.cov = cov
+        self.decay = decay
+
+    def get_param_proposal(self, model: LinearLayer) -> dict:
+        """Get a parameter proposal from current model state."""
+        self.stdev *= self.decay
+
+        proposed_w = mv_norm.rvs(model.w, self.cov)
+        proposed_b = mv_norm.rvs(model.b, self.cov)
 
         return {'w': proposed_w, 'b': proposed_b}
