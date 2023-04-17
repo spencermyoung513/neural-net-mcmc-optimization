6d9a85f447af531728290bd79e5a51c4dc1df304
Spencer Young <spencermyoung513@gmail.com>
2023-04-17 14:22:09 -0600

    Added example with multivariate linear regression

--- proposal_generators.py
+++ proposal_generators.py
@@ -31,6 +31,10 @@ class LinearLayerMultivariateNormalProposalGenerator:
     
     This object creates proposals by drawing from multivariate Gaussians centered at each parameter
     with some user-specified `cov` that controls the covariance in the distribution.
+
+    The `decay` parameter controls how the covariance of the proposal distribution shrinks
+    with time. With each successive call of `get_param_proposal`, self.cov = self.cov * decay.
+    Defaults to 1 (no decaying).
     """
     def __init__(self, cov: float = 1., decay: float = 1.):
         self.cov = cov
@@ -38,7 +42,7 @@ class LinearLayerMultivariateNormalProposalGenerator:
 
     def get_param_proposal(self, model: LinearLayer) -> dict:
         """Get a parameter proposal from current model state."""
-        self.stdev *= self.decay
+        self.cov *= self.decay
 
         proposed_w = mv_norm.rvs(model.w, self.cov)
         proposed_b = mv_norm.rvs(model.b, self.cov)
