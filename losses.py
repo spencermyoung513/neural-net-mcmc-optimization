83649c5fe318e33980f6d2d7c51557005d357dea
maxb700 <brownmax700@gmail.com>
2023-04-18 09:31:36 -0600

    Added losses.py and added new neural network model to models.py

---
+++ losses.py
@@ -0,0 +1,14 @@
+import numpy as np
+from sklearn.metrics import log_loss
+
+def softmax(xs):
+    return np.exp(xs)/(np.sum(np.exp(xs)))
+    
+#Define loss function
+def loss(nn,w1,b1,w2,b2,xs,ys):
+    pred_probs = []
+    N = len(ys)
+    for i,x in enumerate(xs):
+        probs = softmax(nn.forward(x,w1,b1,w2,b2))
+        pred_probs.append(probs)
+    return log_loss(ys,pred_probs,labels=[0,1,2,3,4,5,6,7,8,9])
--- models.py
+++ models.py
@@ -13,3 +13,27 @@ class LinearLayer:
         self.w = w
         self.b = b
         return self
+    
+class two_model:
+    "A two layer neural network with Relu Activation"
+    def __init__(self,w1,b1,w2,b2):
+		if w1 is None:
+        	self.w1 = np.random.normal(loc=0,scale=1.0,size=(30,64))
+        	self.b1 = np.random.normal(loc=0,scale=1.0,size=50)
+        	self.w2 = np.random.normal(loc=0,scale=1.0,size=(10,30))
+        	self.b2 = np.random.normal(loc=0,scale=1.0,size=10)
+		else:
+			self.w1 = w1
+        	self.b1 = b1
+        	self.w2 = w2
+        	self.b2 = b2
+
+    def forward(self,x,w1,b1,w2,b2):
+        x = x.flatten()
+        return w2@np.maximum(w1@x + b1,0)+b2
+
+    def update_weights(self,w1,b1,w2,b2):
+        self.w1 = w1
+        self.b1 = b1
+        self.w2 = w2
+        self.b2 = b2
