feeead4998577d9efe20a2723fe4320e6ef5a391
maxb700 <brownmax700@gmail.com>
2023-04-19 14:57:37 -0600

    Best Params for NN

--- functions.py
+++ functions.py
@@ -2,8 +2,10 @@ import numpy as np
 from sklearn.metrics import log_loss
 
 def softmax(xs,temperature=1e3):
+    if len(xs.shape) == 1: #Increase dimension to fit below for 1 dimension arrays
+        xs = np.expand_dims(xs,0)
     norm_fact = np.max(xs)
-    temperature = norm_fact   #I'm not sure if this is the best way?  
+    temperature = norm_fact   #I'm not sure if this is the best way? Should I do it individually for each set of probabilities?
     return np.exp(xs/temperature)/(np.sum(np.exp(xs/temperature),axis=1).reshape(-1,1))
     
 def cross_entropy_loss(targets,preds):
